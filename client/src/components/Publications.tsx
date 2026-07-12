375c958760fe8fd3ba87e66e7abf4ce6ffd14c6f
tasvesamir07 <tasvesamir15471@gmail.com>
2026-07-12 20:57:25 +0600

    style: reduce publication numbering size to text-lg and font-bold

--- client/src/components/Publications.tsx
+++ client/src/components/Publications.tsx
@@ -141,7 +141,7 @@ const Publications = () => {
                                         )}
                                         <div className="text-left flex-grow flex flex-col justify-center">
                                             <h3 className="text-xl sm:text-2xl font-black text-gray-900 leading-snug mb-4 flex items-start gap-2.5">
-                                                <span className="text-[#8c2626] font-sans font-extrabold text-xl sm:text-2xl select-none leading-none pt-0.5 shrink-0">
+                                                <span className="text-[#8c2626] font-bold text-lg sm:text-xl select-none shrink-0 pt-0.5">
                                                     [{localizeNumbers(pubNumber, language)}]
                                                 </span>
                                                 <span className="flex-1">
